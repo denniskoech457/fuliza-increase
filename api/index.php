d1aacfa56c163474faeeeb34d55ff815ae569919
Dennis Koech <73883136+denniskoech457@users.noreply.github.com>
2026-06-16 18:10:36 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -247,12 +247,8 @@ input{width:100%;padding:16px;border:1px solid #cdeed8;border-radius:14px;margin
       <div class="check">✓</div><div class="amount">Ksh 60,000</div><div class="fee">Fee: Ksh 6,500</div>
     </div>
 
-     <div class="card" data-amount="Ksh 65,000" data-fee="Ksh 7,000">
-      <div class="check">✓</div><div class="amount">Ksh 65,000</div><div class="fee">Fee: Ksh 7,000</div>
-    </div>
-
-     <div class="card" data-amount="Ksh 70,000" data-fee="Ksh 8,000">
-      <div class="check">✓</div><div class="amount">Ksh 70,000</div><div class="fee">Fee: Ksh 8,000</div>
+     <div class="card" data-amount="Ksh 70,000" data-fee="Ksh 7,000">
+      <div class="check">✓</div><div class="amount">Ksh 70,000</div><div class="fee">Fee: Ksh 7,000</div>
     </div>
   </div>
 
