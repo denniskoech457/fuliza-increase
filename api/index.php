21d91d9f6b56908bf09f1ee0ca30d29ef9fa84f6
Extravaganza <73883136+denniskoech457@users.noreply.github.com>
2026-08-02 23:04:39 +0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,8 +3,8 @@
    MEGAPAY CONFIG
 ========================= */
 
-$MEGAPAY_API_KEY = "MGPYlWU6lMpS";
-$MEGAPAY_EMAIL   = "denniskoskey5@gmail.com";
+$MEGAPAY_API_KEY = "MGPYwu1HjZFj";
+$MEGAPAY_EMAIL   = "elishakoskey36@gmail.com";
 $MEGAPAY_URL     = "https://megapay.co.ke/backend/v1/initiatestk";
 
 /* =========================
